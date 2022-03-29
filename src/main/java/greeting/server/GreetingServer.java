d8ff6b6a9885acb3607294395ea7403375f439a2
Ke Deng <dengke0427@gmail.com>
2022-03-29 15:21:56 -0700

    gRPC reflection

--- src/main/java/greeting/server/GreetingServer.java
+++ src/main/java/greeting/server/GreetingServer.java
@@ -2,6 +2,7 @@ package greeting.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 import java.io.IOException;
 
@@ -10,7 +11,10 @@ public class GreetingServer {
     public static void main(String[] args) throws IOException, InterruptedException {
 
 
-        Server server = ServerBuilder.forPort(50051).addService(new GreetServiceImpl()).build();
+        Server server = ServerBuilder.forPort(50051)
+                .addService(new GreetServiceImpl())
+                .addService(ProtoReflectionService.newInstance())
+                .build();
         server.start();
 
         System.out.println("Hello gRPC!");
